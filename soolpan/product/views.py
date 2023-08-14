b900b66c07cad10c4dfb1343b7f81135952abcdd
dilicat0813 <mkh0813@naver.com>
2023-08-14 13:31:24 +0900

    230814_주류평가 페이지 paginator 추가
    페이지네이터 추가

--- soolpan/product/views.py
+++ soolpan/product/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 from DataBase.models import Tal
 from django.views.generic import ListView
+from django.core.paginator import Paginator
+
 
 class ProductList(ListView):
     model = Tal
     template_name = 'product.html'
     context_object_name = 'product_list'
+    paginate_by = 6  # 원하는 페이지당 아이템 수로 변경 가능
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # Paginator 객체 생성
+        paginator = Paginator(self.object_list, self.paginate_by)
+
+        # URL에서 'page' 매개변수를 사용하여 현재 페이지 번호 가져오기
+        page_number = self.request.GET.get('page')
+
+        # 현재 페이지의 Page 객체 가져오기
+        page_obj = paginator.get_page(page_number)
+
+        # context에 페이지 객체 추가
+        context['page_obj'] = page_obj
+
+        return context
